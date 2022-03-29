48cd43c1e87b0ebbea657ea1875ce439ad514606
Lara <54847633+LaraPirkner@users.noreply.github.com>
2022-03-29 09:25:40 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'pinkish': {  DEFAULT: '#FFC2CA',  '50': '#FFFFFF',  '100': '#FFFFFF',  '200': '#FFFFFF',  '300': '#FFFFFF',  '400': '#FFEBED',  '500': '#FFC2CA',  '600': '#FF8A99',  '700': '#FF5269',  '800': '#FF1A38',  '900': '#E1001D'},
+        'woodland': {  DEFAULT: '#4F5D2F',  '50': '#AEC083',  '100': '#A5BA76',  '200': '#93AC5B',  '300': '#7D934A',  '400': '#66783D',  '500': '#4F5D2F',  '600': '#2F381C',  '700': '#101209',  '800': '#000000',  '900': '#000000'},
+        'yelloworange': {  DEFAULT: '#FFBF47',  '50': '#FFFFFF',  '100': '#FFF8EA',  '200': '#FFEAC1',  '300': '#FFDB99',  '400': '#FFCD70',  '500': '#FFBF47',  '600': '#FFAB0F',  '700': '#D68B00',  '800': '#9E6700',  '900': '#664200'},
+        'swampgreen': {  DEFAULT: '#ACC18A',  '50': '#FFFFFF',  '100': '#F8FAF5',  '200': '#E5EBDA',  '300': '#D2DDBF',  '400': '#BFCFA5',  '500': '#ACC18A',  '600': '#92AE65',  '700': '#758F4C',  '800': '#576A38',  '900': '#394625'},
+        'reef': {  DEFAULT: '#DAFEB7',  '50': '#FFFFFF',  '100': '#FFFFFF',  '200': '#FFFFFF',  '300': '#FFFFFF',  '400': '#EFFFDF',  '500': '#DAFEB7',  '600': '#BEFD80',  '700': '#A1FC48',  '800': '#85FC11',  '900': '#69D203'},
+        'halfwhite': {  DEFAULT: '#FDF2D8',  '50': '#FFFFFF',  '100': '#FFFFFF',  '200': '#FFFFFF',  '300': '#FFFFFF',  '400': '#FFFFFF',  '500': '#FDF2D8',  '600': '#FAE0A3',  '700': '#F8CE6D',  '800': '#F5BD38',  '900': '#E9A70C'},
+        'lily': {  DEFAULT: '#C5AABB',  '50': '#FFFFFF',  '100': '#FFFFFF',  '200': '#F7F3F5',  '300': '#E6DBE2',  '400': '#D6C2CE',  '500': '#C5AABB',  '600': '#AE89A0',  '700': '#976786',  '800': '#765168',  '900': '#553A4B'}
+      },
+    },
+  },
+  plugins: [],
+}
